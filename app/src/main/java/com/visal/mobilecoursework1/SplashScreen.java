78c62b5527bb61615ca321ff27dd116a9fe60668
Visal Rajapakse <visalrajapakse@gmail.com>
2020-02-10 13:07:35 +0530

    first commit

---
+++ app/src/main/java/com/visal/mobilecoursework1/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.visal.mobilecoursework1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+//https://www.youtube.com/watch?v=D7c4bZzLVxs
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
